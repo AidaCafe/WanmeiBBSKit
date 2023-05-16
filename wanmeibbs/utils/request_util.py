bc024768278f9ba3023268b4de3cd7b0384eb71d
Wansn <2632324507@qq.com>
2023-05-16 15:25:11 +0800

    🐛 修复: transport未按预期添加url参数

--- wanmeibbs/utils/request_util.py
+++ wanmeibbs/utils/request_util.py
@@ -1,13 +1,13 @@
-from time import time as timestamp
+import time
+from typing import Optional
 
-from httpx import Request, Response
 from httpx import AsyncHTTPTransport, HTTPTransport
+from httpx import Request, Response
 
-from wanmeibbs.utils import Params
 from wanmeibbs.basics import Sha1Utils
 from wanmeibbs.basics.device_generator import get_rand_device
 from wanmeibbs.consts import TigerAPPConsts
-
+from wanmeibbs.utils import Params
 
 __all__ = [
     'TigerTransport',
@@ -15,37 +15,83 @@ __all__ = [
 ]
 
 
+def timestamp():
+    return int(time.time())
+
+
 class AsyncTigerTransport(AsyncHTTPTransport):
-    device_info = get_rand_device()
+    def __init__(
+            self,
+            app_id: Optional[int] = 10021,
+            channel_id: Optional[int] = 1991,
+            sub_app_id: Optional[str] = TigerAPPConsts.PACKAGE_NAME,
+            user_agent: Optional[str] = 'okhttp/4.9.0',
+            **kwargs
+    ):
+        self.device_info = get_rand_device(
+            app_id=app_id,
+            channel_id=channel_id,
+            sub_app_id=sub_app_id
+        )
+        self.user_agent = user_agent
+        super().__init__(**kwargs)
 
     async def handle_async_request(self, request: Request) -> Response:
         params_ = Params(request.url.params)
         current_time = timestamp()
-        params_.update({
+        params_ = params_.merge({
             "timestamp": current_time,
             "deviceInfo": self.device_info,
         })
-        params_["sign"] = Sha1Utils.signParamsString(
-            params=params_.raw,
-            b64_private_key=TigerAPPConsts.RSA_PRIVATE_KEY
+
+        request.url = request.url.copy_merge_params(
+            params_.merge({
+                "sign": Sha1Utils.signParamsString(
+                    params=params_.raw,
+                    b64_private_key=TigerAPPConsts.RSA_PRIVATE_KEY
+                )
+            })
         )
-        request.url.params = request.url.params.merge(params_)
+
+        if not request.headers.get('User-Agent'):
+            request.headers['User-Agent'] = self.user_agent
         return await super().handle_async_request(request=request)
 
 
 class TigerTransport(HTTPTransport):
-    device_info = get_rand_device()
+    def __init__(
+            self,
+            app_id: Optional[int] = 10021,
+            channel_id: Optional[int] = 1991,
+            sub_app_id: Optional[str] = TigerAPPConsts.PACKAGE_NAME,
+            user_agent: Optional[str] = 'okhttp/4.9.0',
+            **kwargs
+    ):
+        self.device_info = get_rand_device(
+            app_id=app_id,
+            channel_id=channel_id,
+            sub_app_id=sub_app_id
+        )
+        self.user_agent = user_agent
+        super().__init__(**kwargs)
 
     def handle_async_request(self, request: Request) -> Response:
         params_ = Params(request.url.params)
         current_time = timestamp()
-        params_.update({
+        params_ = params_.merge({
             "timestamp": current_time,
             "deviceInfo": self.device_info,
         })
-        params_["sign"] = Sha1Utils.signParamsString(
-            params=params_.raw,
-            b64_private_key=TigerAPPConsts.RSA_PRIVATE_KEY
+
+        request.url = request.url.copy_merge_params(
+            params_.merge({
+                "sign": Sha1Utils.signParamsString(
+                    params=params_.raw,
+                    b64_private_key=TigerAPPConsts.RSA_PRIVATE_KEY
+                )
+            })
         )
-        request.url.params = request.url.params.merge(params_)
+
+        if not request.headers.get('User-Agent'):
+            request.headers['User-Agent'] = self.user_agent
         return super().handle_request(request=request)
