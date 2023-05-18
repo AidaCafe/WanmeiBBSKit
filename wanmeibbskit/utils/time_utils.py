99c7e1b9470496d3fa917438185dba7b447bb218
Wansn <2632324507@qq.com>
2023-05-18 12:42:07 +0800

    🦄 实现: 计时器装饰器

--- wanmeibbskit/utils/time_utils.py
+++ wanmeibbskit/utils/time_utils.py
@@ -1,3 +1,6 @@
+import asyncio
+from functools import wraps
+from inspect import iscoroutinefunction
 from time import time as get_time
 
 __all__ = ['timestamp']
@@ -9,3 +12,22 @@ def timestamp() -> int:
     :return: Unix时间戳
     """
     return int(get_time())
+
+
+def timeit(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        func_name = getattr(func, '__name__', str(func))
+        if iscoroutinefunction(func):
+            time_start_ = get_time()
+            result_ = asyncio.run(func(*args, **kwargs))
+            time_end_ = get_time()
+            with asyncio.Lock():
+                print(f'{func_name}: {time_end_ - time_start_}')
+            return result_
+        time_start_ = get_time()
+        result_ = asyncio.run(func(*args, **kwargs))
+        time_end_ = get_time()
+        print(f'{func_name}: {time_end_ - time_start_}')
+        return result_
+    return wrapper
