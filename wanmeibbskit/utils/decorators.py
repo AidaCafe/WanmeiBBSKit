cb97b66d9ca15ba74d35b0a80d345d189917ae27
Wansn <2632324507@qq.com>
2023-05-18 18:36:18 +0800

    🦄 实现: 强制登录装饰器

--- wanmeibbskit/utils/decorators.py
+++ wanmeibbskit/utils/decorators.py
@@ -1,8 +1,34 @@
+import asyncio
 from functools import wraps
+
+__all__ = [
+    'enforce_implementation',
+    'method_need_login'
+]
 
 
 def enforce_implementation(method):
     @wraps(method)
     def wrapper(*args, **kwargs):
         raise NotImplementedError
+
+    return wrapper
+
+
+def method_need_login(func):
+    if asyncio.iscoroutinefunction(func):
+        @wraps(func)
+        async def async_wrapper(self, *args, **kwargs):
+            if not self.isLogin:
+                raise Exception("Login required!")
+            return await func(self, *args, **kwargs)
+
+        return async_wrapper
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if not self.isLogin:
+            raise Exception("Login required!")
+        return func(self, *args, **kwargs)
+
     return wrapper
