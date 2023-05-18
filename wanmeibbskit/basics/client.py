a3d863110e4d1a8dd6b7ca95229e0168f366d824
Wansn <2632324507@qq.com>
2023-05-18 18:40:06 +0800

    🚀 实现: 完整token登录流程

--- wanmeibbskit/basics/client.py
+++ wanmeibbskit/basics/client.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Any, Mapping, Optional, Union
 
 from httpx import AsyncClient
@@ -5,27 +6,43 @@ from httpx import Response
 
 from wanmeibbskit.basics import HYBRID_URL
 from wanmeibbskit.utils import AsyncTigerTransport, URL
-from wanmeibbskit.utils.decorators import enforce_implementation
+from wanmeibbskit.utils import secure_json_retrieve
+from wanmeibbskit.models.device_info import DeviceInfo
 
 
 class WanmeiBBSClient:
+    _uid: Union[None, int] = None
+    _token: Union[None, str] = None
+
     @property
-    def isLogin(self):
+    def isLogin(self) -> bool:
         return self._isLogin
 
-    def __init__(self):
+    @property
+    def device_info(self) -> DeviceInfo:
+        return self._device_info
+
+    @property
+    def token(self):
+        return self._token
+
+    @property
+    def uid(self) -> int:
+        return self._uid
+
+    def __init__(self, device: Optional[DeviceInfo] = DeviceInfo):
+        self._device_info = device
         self.client = AsyncClient(
             base_url=HYBRID_URL.PWCGAPI,
-            transport=AsyncTigerTransport(),
+            transport=AsyncTigerTransport(device_info=self._device_info),
         )
 
         self._isLogin = False
 
     @classmethod
-    @enforce_implementation
-    def from_token(cls, token: str) -> "WanmeiBBSClient":
-        instance_ = cls()
-        instance_.login_data = ...
+    def from_token(cls, uid: int, token: str, device: DeviceInfo) -> "WanmeiBBSClient":
+        instance_ = cls(device=device)
+        asyncio.run(instance_.login(uid=uid, token=token))
         return instance_
 
     async def request(
@@ -41,3 +58,23 @@ class WanmeiBBSClient:
             data=data,
             params=params
         )
+
+    async def login(self, uid: int, token: str):
+        resp_ = await self.client.post(
+            '/login/v2',
+            params={
+                "uid": uid,
+                "username": None,
+                "avatar": None,
+            },
+            headers={
+                "token": token
+            }
+        )
+        data_ = secure_json_retrieve(resp_)
+        if data_ and data_.get('code', -1) == 0:
+            self._isLogin = True
+            self._uid = data_['result'].get('uid')
+            self._token = token
+            return data_
+        raise ValueError('Not a valid login token!')
