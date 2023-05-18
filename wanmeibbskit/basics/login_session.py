b70cc9aa93efbf0b431341ffe5451dc13f9ed133
Wansn <2632324507@qq.com>
2023-05-18 18:39:29 +0800

    🚀 实现: 设备信息持久化

--- wanmeibbskit/basics/login_session.py
+++ wanmeibbskit/basics/login_session.py
@@ -3,9 +3,14 @@ from typing import Optional, Union
 from httpx import AsyncClient
 
 from wanmeibbskit.basics import HYBRID_URL
+from wanmeibbskit.basics.device_generator import get_rand_device
+from wanmeibbskit.consts import DEFAULT_LOGIN_AREA
+from wanmeibbskit.consts import DEFAULT_LOGIN_TYPE
+from wanmeibbskit.consts import TigerAPPConsts
+from wanmeibbskit.models.device_info import DeviceInfo
+from wanmeibbskit.models.sms_login import SmsLoginResponse
 from wanmeibbskit.utils import AsyncUserMgrTransport
 from wanmeibbskit.utils import secure_json_retrieve
-from wanmeibbskit.models.sms_login import SmsLoginResponse
 
 __all__ = ['SmsLoginSession']
 
@@ -14,25 +19,42 @@ class SmsLoginSession:
     def __init__(
             self,
             phone_number: int,
-            app_id: Optional[int] = 10021,
-            channel_id: Optional[int] = 1991,
-            login_type: Optional[int] = 16,
+            device_info: Optional[DeviceInfo] = None,
+            app_id: Optional[int] = None,
+            channel_id: Optional[int] = None,
+            login_type: Optional[int] = None,
             area_code_id: Optional[int] = 1,
             **client_kwargs
     ) -> None:
-        client_kwargs.update({
-            "base_url": HYBRID_URL.USERAPI,
-            "transport": AsyncUserMgrTransport(
+        if not device_info:
+            if not app_id:
+                app_id = TigerAPPConsts.COMMON_APP_ID
+            if not channel_id:
+                channel_id = TigerAPPConsts.COMMON_CHANNEL_ID
+
+            device_info = get_rand_device(
                 app_id=app_id,
                 channel_id=channel_id
             )
-        })
-        self.phone_number = phone_number
-        self.login_type = login_type
-        self.area_code_id = area_code_id
-        self._client = AsyncClient(**client_kwargs)
 
+        self._device_info = device_info
+        self.phone_number = phone_number
+        self.login_type = login_type if login_type else DEFAULT_LOGIN_TYPE
+        self.area_code_id = area_code_id if area_code_id else DEFAULT_LOGIN_AREA
+
+        client_kwargs.update({
+            "base_url": HYBRID_URL.USERAPI,
+            "transport": AsyncUserMgrTransport(
+                device_info=self._device_info
+            )
+        })
+
+        self._client = AsyncClient(**client_kwargs)
         self._sent_count = 0
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        return self._device_info
 
     @property
     def sent_count(self):
