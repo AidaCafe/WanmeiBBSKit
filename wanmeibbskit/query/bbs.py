1762e5dc067200a040fba7f3e9d2131db620f947
Wansn <2632324507@qq.com>
2023-05-19 17:14:40 +0800

    🐛 修复: 未传入token作为header导致的请求错误

--- wanmeibbskit/query/bbs.py
+++ wanmeibbskit/query/bbs.py
@@ -3,18 +3,21 @@ from typing import Union
 from wanmeibbskit.basics.api.pwcg import PerfectWorldAPI
 from wanmeibbskit.utils.safely_getters import secure_json_retrieve
 from wanmeibbskit.utils.decorators import method_need_login
+from wanmeibbskit.models import CommonResponse
+from wanmeibbskit.models.pwcgapi.role_list import RoleList
 
 
 class BBSApp(PerfectWorldAPI):
     @method_need_login
-    async def roleList(self, uid: int) -> Union[dict, None]:
+    async def roleList(self) -> Union[CommonResponse[RoleList], None]:
         resp_ = await self.client.post(
             '/game/roleList',
             params={
-                "uid": uid,
+                "uid": self.uid,
                 "oneAppId": 1256,
+            },
+            headers={
+                "token": self.token
             }
         )
-        print(resp_.headers, resp_.url.params)
-        print(self.device_info)
-        return secure_json_retrieve(resp_, restrict_status_code=True)
+        return CommonResponse[RoleList].parse_obj(secure_json_retrieve(resp_, restrict_status_code=True))
