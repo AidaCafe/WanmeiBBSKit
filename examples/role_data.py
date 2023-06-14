b4b46e57c9e6500de74eea8a15dd4434290559c1
Wansn <2632324507@qq.com>
2023-06-14 17:37:11 +0800

    🦄 重写: example

--- examples/role_data.py
+++ examples/role_data.py
@@ -1,19 +1,31 @@
+import subprocess
 from asyncio import run
+from pathlib import Path
 
 import orjson
 
 from wanmeibbskit.query.hotta import HottaClient
 from wanmeibbskit.models.device_info import DeviceInfo
+from sms_login import main as sms_login
+
+FILE = Path(__file__).parent
+DEVICE_INFO_PATH = FILE / 'device.json'
+LOGIN_STATE_PATH = FILE / 'LOGIN_STATE'
 
 
 async def main():  # 请先运行sms_login.py 短信登录后再试。
+    if not DEVICE_INFO_PATH.exists() or not LOGIN_STATE_PATH.exists():
+        print("暂无登录数据。启动登录流程...")
+        await sms_login()
+
     with open('device.json', 'r') as f:
         device = DeviceInfo.parse_obj(orjson.loads(f.read()))
 
     # 注意短信登录内的uid和token字段，将下面的LOGIN_UID和LOGIN_TOKEN 替换为其对应的值
-    LOGIN_UID = 0
-    LOGIN_TOKEN = "token"
-    client = await HottaClient.from_token(uid=LOGIN_UID, token=LOGIN_TOKEN, device=device)
+    with open('LOGIN_STATE', 'r') as f:
+        uid, token = [_.split('=')[1] for _ in f.readlines()]
+
+    client = await HottaClient.from_token(uid=uid, token=token, device=device)
     resp_ = await client.getRoleData()
     role_data = resp_.result
     print(f"活跃天数{role_data.days}")
--- examples/sms_login.py
+++ examples/sms_login.py
@@ -1,19 +1,23 @@
+from pathlib import Path
+
+import orjson
+
 from wanmeibbskit.basics.login_session import SmsLoginSession
 from wanmeibbskit.models.device_info import DeviceInfo
 
 
-async def main(session: SmsLoginSession):
+async def login(session: SmsLoginSession):
     if not await session.send_sms():
         print('发送失败!')
         return {}
-    while not (cCode := 0):
+    while not (captcha_code := 0):
         input_code = input('验证码: ')
         if not input_code.isdigit() or not len(input_code) != 0:
             print('验证码格式错误')
             return {}
-        cCode = int(input_code)
+        captcha_code = int(input_code)
         break
-    if not await session.verify_code(captcha_code=cCode):
+    if not await session.verify_code(captcha_code=captcha_code):
         print('验证码错误')
         return {}
     return await session.login()
@@ -25,15 +29,10 @@ async def send_only(session: SmsLoginSession):
     return '发送成功'
 
 
-if __name__ == '__main__':
-    import asyncio
-    from pathlib import Path
-
-    import orjson
-
-    while not (PHONE_NUMBER := 0): # 获取手机号
+async def main():
+    while not (phone_number := 0):  # 获取手机号
         if (phone_number_ := input('输入手机号:')).isdigit():
-            PHONE_NUMBER = int(phone_number_)
+            phone_number = int(phone_number_)
             break
         else:
             print('手机号格式错误')
@@ -42,12 +41,28 @@ if __name__ == '__main__':
 
     if not stored_device.exists():  # 如果没有本地设备，则让登录session自动生成并储存
         with open(stored_device, 'wb+') as f:
-            session_ = SmsLoginSession(phone_number=PHONE_NUMBER)  # 短信登录session
-            f.write(orjson.dumps(session_.device_info.dict(by_alias=True), option=orjson.OPT_INDENT_2))
+            session = SmsLoginSession(phone_number=phone_number)  # 短信登录session
+            f.write(orjson.dumps(session.device_info.dict(by_alias=True), option=orjson.OPT_INDENT_2))
     else:
         with open(stored_device, 'r', encoding='utf-8') as f:
             local_device = DeviceInfo.parse_obj(orjson.loads(f.read()))
-            session_ = SmsLoginSession(phone_number=PHONE_NUMBER, device_info=local_device)
+            session = SmsLoginSession(phone_number=phone_number, device_info=local_device)
 
-    data = asyncio.run(main(session_))
-    print(data)
+    data = await login(session)
+
+    print('\n'.join([
+        f'uid: {data.result.userId}',
+        f'token: {data.result.token}',
+        f'昵称: {data.result.nickname}'
+    ]))
+    with open('LOGIN_STATE', 'w+', encoding='utf-8') as f:
+        f.writelines([
+            f'UID={data.result.userId}\n',
+            f'TOKEN={data.result.token}'
+        ])
+        print('登陆状态已保存到LOGIN_STATE文件中。')
+
+if __name__ == '__main__':
+    import asyncio
+
+    asyncio.run(main())
